917716d9b2e2bc5e8f700ff6d0cd9eabe47ab948
aydjay <aydjay@gmail.com>
2019-05-21 10:03:50 +0100

    Modified Project to accomodate unit test

---
+++ Navigator.Tests/Repository/StaticDataRepositoryTest.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Linq;
+using EVEAsset.Navigator.Interfaces;
+using EVEAsset.Navigator.Repositories;
+using EVEStandard.Enumerations;
+using EVEStandard.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace EVEAsset.Navigator.Tests.Repository
+{
+    [TestClass]
+    [TestCategory("Repository")]
+    public class StaticDataRepositoryTest
+    {
+        private static StaticDataRepository _staticDataRepo;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            var mockCache = new Mock<IUniverseCache>();
+            mockCache.Setup(x => x.GetAllByCategory(It.IsAny<CategoryEnum>())).Returns(Systems());
+
+            _staticDataRepo = new StaticDataRepository(mockCache.Object);
+        }
+
+        private static IEnumerable<UniverseIdsToNames> Systems()
+        {
+            return new List<UniverseIdsToNames>
+            {
+                new UniverseIdsToNames {Category = CategoryEnum.solar_system, Id = 1, Name = "Jita"},
+                new UniverseIdsToNames {Category = CategoryEnum.solar_system, Id = 1, Name = "J130719"},
+                new UniverseIdsToNames {Category = CategoryEnum.solar_system, Id = 1, Name = "J230559"},
+                new UniverseIdsToNames {Category = CategoryEnum.solar_system, Id = 1, Name = "Thera"},
+                new UniverseIdsToNames {Category = CategoryEnum.solar_system, Id = 1, Name = "Amarr"}
+            };
+        }
+
+        [TestMethod]
+        public void OnlyGetKSpaceSystems()
+        {
+            Assert.AreEqual(2, _staticDataRepo.GetAllSystems().Count());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# EVEAsset.Navigator
+
+Small project aiming to solve 2 problems for people that live in WSpace.
+
+1) How far are K-Space connections to a particular systems
+2) How far are assets from K-Space connections.
+
+This may be particularily useful for dwellers of Thera.
