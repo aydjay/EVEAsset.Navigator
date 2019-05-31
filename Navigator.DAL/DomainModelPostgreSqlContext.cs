4baa7c6298f7d51d7515886eca8cf060286446b6
aydjay <aydjay@gmail.com>
2019-05-31 14:08:31 +0100

    Add DAL layer

---
+++ Navigator.DAL/DomainModelPostgreSqlContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Navigator.DAL
+{
+    //Taken from https://damienbod.com/2016/01/11/asp-net-5-with-postgresql-and-entity-framework-7/
+    public class DomainModelPostgreSqlContext : DbContext
+    {
+        public DomainModelPostgreSqlContext(DbContextOptions<DomainModelPostgreSqlContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public override int SaveChanges()
+        {
+            ChangeTracker.DetectChanges();
+
+            return base.SaveChanges();
+        }
+    }
+}
