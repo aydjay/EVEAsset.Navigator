f7fa45f437e063efc81ec5614ba712fa4ebc5ff0
Alex Jones <alex.jones@certara.com>
2019-12-06 13:19:59 +0000

    Initial Commit of EveScout cacheable entities

---
+++ Navigator.DAL/Navigator/ScoutData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Navigator.DAL.Navigator
+{
+    public class ScoutData
+    {
+        /// <summary>
+        ///     SolarSystemID
+        /// </summary>
+        public int Id { get; set; }
+
+        public string SolarSystemName { get; set; }
+
+        public DateTime CachedTime { get; set; }
+
+        public DateTime EarliestExpiryTime { get; set; }
+
+        /// <summary>
+        /// EvescoutDto that comes over the wire
+        /// </summary>
+        public string EveScoutData { get; set; }
+    }
+}
